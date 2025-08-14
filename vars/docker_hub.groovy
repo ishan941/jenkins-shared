96fd8e43588160d7b3fd375142f0e385b589eee4
Ishan Shrestha <iahan.esan18@gmail.com>
2025-08-14 21:54:11 +0545

    fix: added git-clone

--- vars/docker_hub.groovy
+++ vars/docker_hub.groovy
@@ -1,9 +1,10 @@
 def call(String projectName, String tag){
-     withCredentials([usernamePassword(credentialsId: 'dockerHub', usernameVariable: 'dockerHubUser', passwordVariable: 'dockerHubPass')]){
-                    sh '''
-                      docker login -u "$dockerHubUser" -p "$dockerHubPass"
-                      docker push ${dockerHubUser}/${projectName}:${tag}
-                      docker logout
-                      '''
-                }
+    withCredentials([usernamePassword(credentialsId: 'dockerHub', usernameVariable: 'dockerHubUser', passwordVariable: 'dockerHubPass')]){
+        sh """
+            echo "$dockerHubPass" | docker login -u "$dockerHubUser" --password-stdin
+            docker tag $dockerHubUser/$projectName:latest $dockerHubUser/$projectName:$tag
+            docker push $dockerHubUser/$projectName:$tag
+            docker logout
+        """
+    }
 }
